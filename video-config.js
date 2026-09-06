e3d8ed0f5e162ce24a5e2548e91b3fad91f32f3c
8601free-cloud <8601free@gmail.com>
2026-09-06 12:31:00 +0900

    Add files via upload

---
+++ video-config.js
@@ -0,0 +1,5 @@
+window.THE_SUWON_VIDEO_CONFIG = {
+  supabaseUrl: 'https://lfnreutlwezdkpzqgqfi.supabase.co',
+  supabaseAnonKey: 'sb_publishable_teQk9TxtVO_LNoA4Kww9ZA_9MBi3eSV',
+  adminUserId: 'c68fb0fe-3d0c-4caa-92ad-255e5a467d15'
+};
